c40d5b1a8ecab58b2465bf4908df3b8ca09b8628
Илон <vlasovvdm@gmail.com>
2024-04-10 11:50:58 +0400

    !

---
+++ src/main/java/com/example/democalculatorsky/DemocalculatorskyApplication.java
@@ -0,0 +1,13 @@
+package com.example.democalculatorsky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemocalculatorskyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemocalculatorskyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/democalculatorsky/DemocalculatorskyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.democalculatorsky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemocalculatorskyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
